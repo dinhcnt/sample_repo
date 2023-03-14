3e76c41f5abdb73b6237a4d1fdf027abd500483d
Christina Dinh <christinadinh@Christinas-MBP.lan>
2023-03-14 16:34:12 -0700

    delete albums.py and song.py

--- album.py
+++
@@ -1,5 +0,0 @@
-def find_by_name(album, name):
-    return [album for albums where album['name'] == name]
-
-def find_albums(albums, title):
-    return [album for album in albums if album['title'] == 'title']
